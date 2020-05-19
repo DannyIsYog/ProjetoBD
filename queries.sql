58797a7326307e7e3e2c076604f67efd9ea953fc
joaoecsde <48735611+joaoecsde@users.noreply.github.com>
2020-05-19 16:58:28 +0100

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -11,12 +11,12 @@ having count(*) >= all (
 2.
 
 select zona, count(*)
-from anomalia natural join anomalia_tradução
+from anomalia natural join anomalia_traducao
 group by zona
 where ts >= '2020-01-01 00:00:00' and ts <= '2020-06-30 23:59:59'
 having count(*) <= all (
 	select count(*)
-	from anomalia join anomalia_tradução
+	from anomalia join anomalia_traducao
 	group by zona
 	where ts >= '2020-01-01 00:00:00' and ts <= '2020-06-30 23:59:59');
 
@@ -24,17 +24,13 @@ having count(*) <= all (
 
 3.
 
-select c.email 
-from  correção c natural join incidência i natural join item m
+select c.email
+from  correcao c natural join incidencia i natural join item m
 where latitude < 39.336775;
 
 
 4.
 
 select c.email
-from correção c natural join incidencia i
+from correcao c natural join incidencia i
 where c.email = i.email
- 
-
-
-
